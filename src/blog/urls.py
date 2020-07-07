28e5721d4d53c4e26d46324427c0250d9b42cbfe
Dominic Catana <catanadominic@gmail.com>
2020-07-07 13:50:01 +0300

    template tag + /tag/ change url name

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -7,5 +7,5 @@ from blog.views import BlogIndex, BlogDetail, ListPostsByTag
 urlpatterns = [
     path(r'', BlogIndex.as_view(), name='blog_index'),  # Blog Index
     path(r'<slug>', BlogDetail.as_view(), name="entry_detail"),  # Blog Detail
-    path(r'posts_by/<str:tag>', ListPostsByTag.as_view(),name='list_by_tag'),
+    path(r'tag/<str:tag>', ListPostsByTag.as_view(),name='list_by_tag'),
 ]
